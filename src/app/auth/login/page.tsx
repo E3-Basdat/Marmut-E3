faafeb322c1493cb1fb8ed986d221d28a1c72b45
venedictchen <venedictchen@gmail.com>
2024-04-25 13:11:12 +0700

    fix: add button register

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,7 +1,7 @@
 const login: React.FC = () => {
     return (
         <div className='flex flex-col text-black text-center items-center gap-16 px-8 py-24 bg-white font-bold min-h-screen '>
-            <p className='text-3xl'>Login</p>
+            <p className='text-3xl'>Please Login</p>
 
             <form className='flex flex-col w-1/2 ' >
                 <div className='mb-4 '>
@@ -14,10 +14,11 @@ const login: React.FC = () => {
                 <div className='mb-4 '>
                 </div>
 
-                <div className='flex flex-row gap-4'>
-
-                    <button type="submit" className="bg-primary text-white bg-[#5038BC] font-semibold
-           rounded-lg w-full py-4 mt-4">
+                <div className='flex flex-row gap-4 justify-center'>
+                    <button type="submit" className="bg-primary text-white bg-[#355c7d] font-semibold rounded-lg w-1/4 py-4 mt-4">
+                        Register
+                    </button>
+                    <button type="submit" className="bg-primary text-white bg-[#355c7d] font-semibold rounded-lg w-1/4 py-4 mt-4">
                         Login
                     </button>
                 </div>
