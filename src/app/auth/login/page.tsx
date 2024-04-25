0c216cca4bdbd324854ea3ef5ce16e2126d5f4f7
venedictchen <venedictchen@gmail.com>
2024-04-25 13:03:17 +0700

    feat: add login page ui

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,12 +1,31 @@
-
-
 const login: React.FC = () => {
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
-    );
-}
+        <div className='flex flex-col text-black text-center items-center gap-16 px-8 py-24 bg-white font-bold min-h-screen '>
+            <p className='text-3xl'>Login</p>
 
+            <form className='flex flex-col w-1/2 ' >
+                <div className='mb-4 '>
+                    <input type="text" placeholder="Email" className="border-2 border-gray-200 rounded-lg w-full py-4 px-4" />
+                </div>
+
+                <div className='mb-4 '>
+                    <input type="password" placeholder="Password" className="border-2 border-gray-200 rounded-lg w-full py-4 px-4" />
+                </div>
+                <div className='mb-4 '>
+                </div>
+
+                <div className='flex flex-row gap-4'>
+
+                    <button type="submit" className="bg-primary text-white bg-[#5038BC] font-semibold
+           rounded-lg w-full py-4 mt-4">
+                        Login
+                    </button>
+                </div>
+
+
+            </form>
+        </div>
+    )
+}
 
 export default login;
