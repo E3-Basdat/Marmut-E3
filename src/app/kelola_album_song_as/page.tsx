fa8f7ff92ec7008e5bc8d4971375a713324c3e06
axelrori <axelrori204@gmail.com>
2024-05-01 19:24:42 +0700

    fitur kelola album song as main page done

---
+++ src/app/kelola_album_song_as/page.tsx
@@ -0,0 +1,53 @@
+"use client"
+import { useRouter } from "next/navigation";
+
+const kelola_album_song_as: React.FC = () => {
+    const router = useRouter();
+    return (
+        <div className="flex min-h-screen bg-white text-white-100 flex-col items-center gap-16 font-bold p-48">
+            <h1 className="text-3xl">List Album</h1>
+            <table className="table-fixed w-full">
+                <thead>
+                    <tr>
+                        <th className="w-1/4 px-4 py-2">Judul</th>
+                        <th className="w-1/4 px-4 py-2">Label</th>
+                        <th className="w-1/4 px-4 py-2">Jumlah Lagu</th>
+                        <th className="w-1/4 px-4 py-2">Total Durasi</th>
+                        <th className="w-1/4 px-4 py-2">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="border px-4 py-2">Speak Now</td>
+                        <td className="border px-4 py-2">Label1</td>
+                        <td className="border px-4 py-2">7</td>
+                        <td className="border px-4 py-2">31</td>
+                        <td className="border px-4 py-2">
+                            <button onClick={() => router.push('/kelola_album_song_as/daftar_lagu')} className="bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded">Lihat Daftar Lagu</button>
+                            <button onClick={() => router.push('/kelola_album_song_as/create_lagu')} className="bg-green-500 hover:bg-green-700 text-white py-1 px-2 rounded ml-2">Tambah Lagu</button>
+                            <button className="bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded ml-2">Hapus</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="border px-4 py-2">City of Evil</td>
+                        <td className="border px-4 py-2">Label2</td>
+                        <td className="border px-4 py-2">6</td>
+                        <td className="border px-4 py-2">30</td>
+                        <td className="border px-4 py-2">
+                            <button onClick={() => router.push('/kelola_album_song_as/daftar_lagu')} className="bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded">Lihat Daftar Lagu</button>
+                            <button onClick={() => router.push('/kelola_album_song_as/create_lagu')} className="bg-green-500 hover:bg-green-700 text-white py-1 px-2 rounded ml-2">Tambah Lagu</button>
+                            <button className="bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded ml-2">Hapus</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <div className="flex flex-col gap-8 w-full justify-center items-center">
+                <button onClick={() => router.push('/kelola_album_song_as/create_album')} className="bg-blue-500 hover:bg-blue-700 text-white font-semibold rounded-lg py-4 w-1/4">
+                    Tambah Album
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default kelola_album_song_as;
